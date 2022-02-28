309c679f745cca89cff72f6fb3b07c450aeeb549
Ellinor Henriksson <eh224kr@student.lnu.se>
2022-02-28 15:51:50 +0100

    Create server.js and mongoose config

---
+++ src/config/mongoose.js
@@ -0,0 +1,29 @@
+/**
+ * Mongoose configuration.
+ *
+ * @author Ellinor Henriksson
+ * @version 1.0.0
+ */
+import mongoose from 'mongoose'
+
+/**
+ * Establishes a connection to a database.
+ *
+ * @returns {Promise} Resolves to this if connection succeeded.
+ */
+export const connectDB = async () => {
+  const { connection } = mongoose
+
+  connection.on('connected', () => console.log('MongoDB connection opened.'))
+  connection.on('error', err => console.error(`MongoDB connection error occurred: ${err}`))
+  connection.on('disconnected', () => console.log('MongoDB is disconnected.'))
+
+  process.on('SIGINT', () => {
+    connection.close(() => {
+      console.log('MongoDB disconnected due to application termination.')
+      process.exit(0)
+    })
+  })
+
+  return mongoose.connect(process.env.DB_CONNECTION_STRING)
+}
